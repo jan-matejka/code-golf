6d7981b518a2e5a97300a38cdbc079dba9e52386
Jan Matějka <jan@matejka.ninja>
2024-07-12 04:38:42 +0200

    postgres-mq/python/main.py: write to postgres queue

--- postgres-mq/python/main.py
+++ postgres-mq/python/main.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python3
 
 import os, sys, time, asyncio
+import psycopg # current debian stable = 3.1.7
 from subprocess import Popen, PIPE, DEVNULL
 
 class Protocol(asyncio.SubprocessProtocol):
-    def __init__(self, i, total):
+    def __init__(self, i, total, exit_future):
         self.i = i
         self.total = total
+        self.exit = exit_future
+        self.exited = False
+        self.pipe_closed = False
+
+    def process_exited(self):
+        self.exited = True
+        self.check_for_exit()
+
+    def pipe_connection_lost(self, fd, exc):
+        self.pipe_closed = True
+        self.check_for_exit()
+
+    def check_for_exit(self):
+        if self.exited and self.pipe_closed:
+            self.exit.set_result(True)
 
     def print_error(self, x):
         print(f"{self.i}: {x}", file=sys.stderr)
@@ -20,8 +36,15 @@ class Protocol(asyncio.SubprocessProtocol):
             self.print_error(f"{fd}: {data!r}")
 
 def gen():
-    while True:
-        sys.stdout.write(".")
+    with psycopg.connect("dbname=mq user=mq host=localhost") as conn:
+        i = 0
+        while True:
+            with conn.cursor() as c:
+                c.execute('insert into public.queue (data) values (%s)', (i,))
+                conn.commit()
+            sys.stdout.write(".")
+            sys.stdout.flush()
+            i += 1
 
 def main():
     """
@@ -31,19 +54,32 @@ def main():
     """
     async def fx():
         total = {}
+        exits = {}
         ts = []
+
+        # start subprocesses
         for i in range(1, 3):
             total[i] = 0
             loop = asyncio.get_running_loop()
+            exits[i] = asyncio.Future(loop=loop)
             t, _ = await loop.subprocess_exec(
-                lambda: Protocol(i, total),
+                lambda: Protocol(i, total, exits[i]),
                 __file__, stdout=PIPE, stderr=sys.stderr, env={"CHILD": str(i)}
             )
             ts.append(t)
+
+        # let them run for about a second
         await asyncio.sleep(1)
+
+        # terminate children
         for t in ts:
             t.close()
 
+        # wait for the termination
+        for d in exits.values():
+            await d
+
+        # print results
         for k, v in total.items():
             print(f"{k}: {v}")
         print(f"total: {sum(total.values())}")
