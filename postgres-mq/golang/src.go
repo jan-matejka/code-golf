99d429dac11a1f63e6f71b983c076d9021472af2
Jan Matějka <jan@matejka.ninja>
2024-07-12 13:14:08 +0200

    postgres-mq/golang/src.go: spawn workers

--- postgres-mq/golang/src.go
+++ postgres-mq/golang/src.go
@@ -2,7 +2,9 @@ package main
 
 import "fmt"
 import "os"
+import "sync"
 import "context"
+import "time"
 import "github.com/jackc/pgx/v4/pgxpool"
 
 func die(message string, args ...interface{}) {
@@ -10,19 +12,12 @@ func die(message string, args ...interface{}) {
 	os.Exit(1)
 }
 
-func main() {
-	pool, err := pgxpool.Connect(context.Background(), "postgres://mq@localhost/mq")
-	if err != nil {
-		die("Unable to connect to database: %v\n", err)
-	}
-	defer pool.Close()
-
+func insert(pool *pgxpool.Pool, i int) {
 	tx, err := pool.Begin(context.Background())
 	if err != nil {
 		die("Tx failed: %v\n", err)
 	}
 
-	var i int64
 	_, err = tx.Exec(context.Background(), "insert into queue (data) values ($1)", fmt.Sprintf("%d", i))
 	if err != nil {
 		die("exec failed %v\n", err)
@@ -32,6 +27,44 @@ func main() {
 	if err != nil {
 		die("failed commit %v\n", err)
 	}
+}
 
+func worker(id int, quit chan bool, wg *sync.WaitGroup, pool *pgxpool.Pool) {
+	defer wg.Done()
+	var i int
+	for {
+		select {
+		case <-quit:
+			return
+		default:
+			insert(pool, i)
+			i++
+		}
+	}
+}
+
+func main() {
+	pool, err := pgxpool.Connect(context.Background(), "postgres://mq@localhost/mq")
+	if err != nil {
+		die("Unable to connect to database: %v\n", err)
+	}
+	defer pool.Close()
+	var wg sync.WaitGroup
+	var quit_channels [4]chan bool
+
+	for i := range quit_channels {
+		wg.Add(1)
+		quit_channels[i] = make(chan bool)
+		go worker(i, quit_channels[i], &wg, pool)
+	}
+
+	time.Sleep(time.Second)
+
+	for i := range quit_channels {
+		quit_channels[i] <- true
+	}
+
+	print("waiting")
+	wg.Wait()
 	os.Exit(0)
 }
