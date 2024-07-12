7b43c8ca9b37c0798b2fdb8c34215d4e2f1dd368
Jan Matějka <jan@matejka.ninja>
2024-07-12 12:46:38 +0200

    postgres-mq/golang: insert into queue

--- postgres-mq/golang/src.go
+++ postgres-mq/golang/src.go
@@ -1,7 +1,37 @@
 package main
 
 import "fmt"
+import "os"
+import "context"
+import "github.com/jackc/pgx/v4/pgxpool"
+
+func die(message string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, message, args...)
+	os.Exit(1)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	pool, err := pgxpool.Connect(context.Background(), "postgres://mq@localhost/mq")
+	if err != nil {
+		die("Unable to connect to database: %v\n", err)
+	}
+	defer pool.Close()
+
+	tx, err := pool.Begin(context.Background())
+	if err != nil {
+		die("Tx failed: %v\n", err)
+	}
+
+	var i int64
+	_, err = tx.Exec(context.Background(), "insert into queue (data) values ($1)", fmt.Sprintf("%d", i))
+	if err != nil {
+		die("exec failed %v\n", err)
+	}
+
+	err = tx.Commit(context.Background())
+	if err != nil {
+		die("failed commit %v\n", err)
+	}
+
+	os.Exit(0)
 }
