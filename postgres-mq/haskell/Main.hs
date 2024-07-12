eeec31e9f6075575c2ef007fef509d9e78b11ef2
Jan Matějka <jan@matejka.ninja>
2024-07-12 15:48:10 +0200

    postgres-mq/haskell: insert row

--- postgres-mq/haskell/Main.hs
+++ postgres-mq/haskell/Main.hs
@@ -1,1 +1,8 @@
-main = putStrLn "Hello"
+{-# LANGUAGE OverloadedStrings #-}
+
+import Database.PostgreSQL.Simple
+
+main = do
+  conn <- connectPostgreSQL "postgres://mq@localhost/mq"
+  execute conn "insert into public.queue (data) values (?)" $ Only ("1"::String)
+  putStrLn "Done"
