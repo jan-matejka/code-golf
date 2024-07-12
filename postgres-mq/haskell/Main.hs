43efa047a3d930179c7386b5370ce3a69045e500
Jan Matějka <jan@matejka.ninja>
2024-07-12 18:44:45 +0200

    postgres-mq/haskell: run a worker forever
    Until the main thread shuts it down via MVar

--- postgres-mq/haskell/Main.hs
+++ postgres-mq/haskell/Main.hs
@@ -1,8 +1,38 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, FlexibleContexts #-}
 
+import Control.Concurrent
+import Control.Concurrent.MVar
+import Control.Monad.Loops
+import Data.Maybe
 import Database.PostgreSQL.Simple
 
-main = do
+insert :: Connection -> Int -> IO Int
+insert conn i = do
+  execute conn "insert into public.queue (data) values (?)" $ Only $ show i
+  return i
+
+worker :: MVar Bool -> MVar Int -> IO ()
+worker quit result = do
   conn <- connectPostgreSQL "postgres://mq@localhost/mq"
-  execute conn "insert into public.queue (data) values (?)" $ Only ("1"::String)
+  let checkQuitAndInsert x = do
+        q <- tryTakeMVar quit
+        case q of
+          Nothing -> do
+              insert conn x
+              return False
+          Just _ -> return True
+  x <- firstM checkQuitAndInsert [0..]
+  putMVar result $ x_or_zero x
+  where
+    x_or_zero Nothing = 0
+    x_or_zero (Just x) = x
+
+main = do
+  quit <- newEmptyMVar
+  result <- newEmptyMVar :: IO (MVar Int)
+  forkIO $ worker quit result
+  threadDelay (10^6::Int)
+  putMVar quit True
+  n <- takeMVar result
+  putStrLn $ show (n::Int)
   putStrLn "Done"
