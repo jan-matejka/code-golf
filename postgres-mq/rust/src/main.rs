3a0067df8005b365c6723aa28988a9e64560f51e
Jan Matějka <jan@matejka.ninja>
2024-07-20 15:57:51 +0200

    postgres-mq/rust: insert row into postgres

--- postgres-mq/rust/src/main.rs
+++ postgres-mq/rust/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use postgres::{Client, NoTls, Error};
+
+fn insert(i: u64) -> Result<(),Error> {
+    let mut client = Client::connect("postgres://mq@localhost/mq", NoTls)?;
+
+    let i_sql = i.to_string();
+    client.execute("insert into queue (data) values ($1)", &[&i_sql])?;
+    return Ok(())
+}
+fn main() -> Result<(), Error> {
+    insert(1)?;
+    return Ok(());
 }
