4fd462787906dfd228c5c5efc5c28822b8b14703
Jan Matějka <jan@matejka.ninja>
2024-07-20 17:14:18 +0200

    postgres-mq/rust: start multiple workers

--- postgres-mq/rust/src/main.rs
+++ postgres-mq/rust/src/main.rs
@@ -1,13 +1,29 @@
-use postgres::{Client, NoTls, Error};
+use postgres as pg;
+use std::thread;
 
-fn insert(i: u64) -> Result<(),Error> {
-    let mut client = Client::connect("postgres://mq@localhost/mq", NoTls)?;
+fn insert(i: u64) -> Result<(),pg::Error> {
+    let mut client = pg::Client::connect("postgres://mq@localhost/mq", pg::NoTls)?;
 
     let i_sql = i.to_string();
     client.execute("insert into queue (data) values ($1)", &[&i_sql])?;
     return Ok(())
 }
-fn main() -> Result<(), Error> {
+
+fn worker() -> Result<(), pg::Error> {
     insert(1)?;
+    return Ok(());
+}
+
+fn main() -> Result<(), pg::Error> {
+    let mut handles = Vec::new();
+    for _ in 1..10 {
+        let h = thread::spawn(worker);
+        handles.push(h);
+    }
+
+    for v in handles {
+        v.join().unwrap()?;
+    }
+
     return Ok(());
 }
