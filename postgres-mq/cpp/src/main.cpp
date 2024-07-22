fd429ab963af06dcdce4b8750c8eb2f8cbf22633
Jan Matějka <jan@matejka.ninja>
2024-07-22 14:53:52 +0200

    postgres-mq/cpp: insert into db

--- postgres-mq/cpp/src/main.cpp
+++ postgres-mq/cpp/src/main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <pqxx/pqxx>
+
+using namespace std;
+using namespace pqxx;
+
+void insert(connection &C, int i) {
+  work W(C);
+  W.exec("insert into queue (data) values (" + W.quote(i) + ")");
+  W.commit();
+}
 
 int main(void) {
-  std::cout << "Hello World\n";
+  try {
+      connection C("postgres://mq@localhost/mq");
+      if (!C.is_open()) {
+         cerr << "Can't open database" << endl;
+         return 1;
+      }
+
+      insert(C, 1);
+
+      C.disconnect ();
+   } catch (const std::exception &e) {
+      cerr << e.what() << std::endl;
+      return 1;
+   }
+
+   return 0;
 }
