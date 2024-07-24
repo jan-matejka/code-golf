b673b38f93d8d6a4ff18796659ade79292b13a24
Jan Matějka <jan@matejka.ninja>
2024-07-24 15:24:40 +0200

    postgres-mq/cpp: start 2 workers dynamically

--- postgres-mq/cpp/src/main.cpp
+++ postgres-mq/cpp/src/main.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 #include <chrono>
 #include <queue>
+#include <ranges>
 
 using namespace std;
 using namespace pqxx;
@@ -13,7 +14,7 @@ void insert(connection &C, int i) {
   W.commit();
 }
 
-void worker(queue<bool> &q) {
+void worker(shared_ptr<queue<bool>> &q) {
   try {
     connection C("postgres://mq@localhost/mq");
     if (!C.is_open()) {
@@ -22,7 +23,7 @@ void worker(queue<bool> &q) {
     }
 
     int i;
-    for (i=0;q.empty();i++) {
+    for (i=0;q->empty();i++) {
       insert(C, i);
     }
 
@@ -34,11 +35,30 @@ void worker(queue<bool> &q) {
   }
 }
 
+void sample_workers(int n) {
+  std::vector<shared_ptr<queue<bool>>> queues;
+  std::vector<shared_ptr<jthread>> threads;
+
+  // for (auto _ : ranges::views::iota(0)) {
+  for(int i=0; i<n; i++)  {
+    auto q = make_shared<queue<bool>>();
+    queues.push_back(q);
+    shared_ptr<jthread> w = make_shared<jthread>(worker, ref(q));
+    threads.push_back(w);
+  }
+
+  for(auto i : ranges::views::iota(0, 3)) {
+    this_thread::sleep_for(chrono::seconds(1));
+    cout << i << "\n";
+  }
+
+  for(auto& q : queues) {
+    q->push(true);
+  }
+}
+
 int main(void) {
-  queue<bool> q;
-  jthread w(worker, ref(q));
-  this_thread::sleep_for(chrono::seconds(3));
-  q.push(true);
+  sample_workers(2);
 
   return 0;
 }
