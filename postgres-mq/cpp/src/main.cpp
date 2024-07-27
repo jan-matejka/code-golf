0a90c7121c9df91c386e754d39d234eafc279e69
Jan Matějka <jan@matejka.ninja>
2024-07-27 17:21:00 +0200

    postgres-mq/cpp: handle igetenv properly

--- postgres-mq/cpp/src/main.cpp
+++ postgres-mq/cpp/src/main.cpp
@@ -12,6 +12,8 @@
 #include <memory>
 #include <optional>
 
+#include <boost/algorithm/string.hpp>
+
 #define VERBOSE(x) if(igetenv("VERBOSE", 0)) osyncstream(cout) << x << endl
 #define INFO(x) osyncstream(cout) << x << endl
 #define ERR(x) osyncstream(cerr) << x << endl
@@ -19,11 +21,33 @@
 #define WERR(id, x) ERR("Worker " << id << ": " << x)
 #define THROW(x) { stringstream ss; ss << x; throw runtime_error(ss.str()); }
 
-#define igetenv(x, def) getenv(x) ? stoi(getenv(x)) : def
-
 using namespace std;
 using namespace pqxx;
 
+inline int igetenv(const char* x, int def) {
+  char *c = getenv(x);
+  if(!c)
+    return def; // x undefined
+
+  string s(c);
+  boost::trim(s);
+  if (s.empty()) // x defined empty
+    return def;
+
+  // x defined with some value
+  try {
+    int i = stoi(s);
+    return i;
+  }catch(const invalid_argument& e) {
+    stringstream s;
+    s << "invalid environemnt variable: " << x;
+    throw runtime_error(s.str());
+  }catch(const out_of_range& e) {
+    stringstream s;
+    s << "out of bounds environment variable:" << x;
+    throw runtime_error(s.str());
+  }
+}
 
 class Worker {
   int worker_id;
