4598e5629e5daf3446a68bfec68b22b9b1f5dc51
Jan Matějka <jan@matejka.ninja>
2024-09-13 23:23:52 +0200

    feat(message-queue/python): define actual metrics

--- message-queue/python/prometheus.py
+++ message-queue/python/prometheus.py
@@ -12,6 +12,24 @@ test_metric = Gauge(
     registry=registry,
 )
 
+messages_total = Gauge(
+    'messages_total', 'Messages sent',
+    Runtime_labels + ('worker_id', 'n_workers', 'algorithm', 'mq_system'),
+    registry=registry,
+)
+
+messages_per_second = Gauge(
+    'messages_per_second', 'Messages per second sent',
+    Runtime_labels + ('worker_id', 'n_workers', 'algorithm', 'mq_system'),
+    registry=registry,
+)
+
+duration_seconds = Gauge(
+    'duration_seconds', 'Work duration in seconds',
+    Runtime_labels + ('worker_id', 'n_workers', 'algorithm', 'mq_system'),
+    registry=registry,
+)
+
 class Pusher:
     def __init__(self, c: "Config"):
         self.config = c
