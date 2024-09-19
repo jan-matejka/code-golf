30f32823724f1b5342ad95818c22bac298003788
Jan Matějka <jan@matejka.ninja>
2024-09-19 15:09:05 +0200

    feat(message-queue): add postgres exporter to prometheus

--- message-queue/container/base.mk
+++ message-queue/container/base.mk
@@ -13,12 +13,12 @@ pull: ## Pull the base image
 .PHONY: run
 run: ## Run the container for local development
 
-	podman run --name $(NAME) -p 127.0.0.1:$(PORT):$(PORT) $(RUN_NAME)
+	podman run --name $(NAME) -p 127.0.0.1:$(PORT):$(PORT) $(RUN_ARGS) $(RUN_NAME)
 
 .PHONY: podrun
 podrun: ## Run the container in a pod
 
-	podman run -d --name $(NAME) --pod mq $(RUN_NAME)
+	podman run -d --name $(NAME) --pod mq $(RUN_ARGS) $(RUN_NAME)
 
 .PHONY: rm
 rm: ## Delete container
