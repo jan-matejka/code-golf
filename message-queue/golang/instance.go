86b789dd033e5e984f831d257f4b406c6626e261
Jan Matějka <jan@matejka.ninja>
2024-09-20 12:51:42 +0200

    feat(message-queue/golang): add Runtime

--- message-queue/golang/instance.go
+++ message-queue/golang/instance.go
@@ -1,14 +1,24 @@
 package golang
 
+import "runtime"
+import "time"
+import "github.com/google/uuid"
 import "github.com/prometheus/client_golang/prometheus/push"
+import "github.com/bensallen/modscan/pkg/uname"
 
 type Instance struct {
+	Runtime    *Runtime
 	Config     *Config
 	Prometheus *push.Pusher
 }
 
 func NewInstance() (*Instance, error) {
+	runtime, err := NewRuntime()
+	if err != nil {
+		return nil, err
+	}
 	i := new(Instance)
+	i.Runtime = runtime
 	c, err := NewConfig()
 	if err != nil {
 		return nil, err
@@ -17,4 +27,40 @@ func NewInstance() (*Instance, error) {
 
 	i.Prometheus = NewPusher()
 	return i, nil
+}
+
+type Runtime struct {
+	ctime        time.Time
+	uuid         uuid.UUID
+	lang         string
+	lang_version string
+	runtime      string
+	os           string
+	kernel       string
+	arch         string
+}
+
+func NewRuntime() (*Runtime, error) {
+	uuid, err := uuid.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+
+	uname, err := uname.New()
+	if err != nil {
+		return nil, err
+	}
+
+	r := &Runtime{
+		time.Now(),
+		uuid,
+		"golang",
+		runtime.Version(),
+		"golang",
+		runtime.GOOS,
+		uname.Release(),
+		runtime.GOARCH,
+	}
+
+	return r, nil
 }
