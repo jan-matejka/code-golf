13a8741e2ad7d00c0469023f90d36b4908e3ef9a
Jan Matějka <jan@matejka.ninja>
2024-09-20 17:34:55 +0200

    refactor(message-queue/cpp): extract log.cpp

---
+++ message-queue/cpp/src/log.cpp
@@ -0,0 +1,40 @@
+#ifndef LOG_CPP
+#define LOG_CPP
+#include <iostream>
+#include <syncstream>
+#include <boost/algorithm/string.hpp>
+
+#define VERBOSE(x) if(igetenv("VERBOSE", 0)) osyncstream(cout) << x << endl
+#define INFO(x) osyncstream(cout) << x << endl
+#define ERR(x) osyncstream(cerr) << x << endl
+#define WVERBOSE(id, x) VERBOSE("Worker " << id << ": " << x)
+#define WERR(id, x) ERR("Worker " << id << ": " << x)
+#define THROW(x) { stringstream ss; ss << x; throw runtime_error(ss.str()); }
+
+using namespace std;
+
+inline int igetenv(const char* x, int def) {
+  char *c = getenv(x);
+  if(!c)
+    return def; // x undefined
+
+  string s(c);
+  boost::trim(s);
+  if (s.empty()) // x defined empty
+    return def;
+
+  // x defined with some value
+  try {
+    int i = stoi(s);
+    return i;
+  }catch(const invalid_argument& e) {
+    stringstream s;
+    s << "invalid environemnt variable: " << x;
+    throw runtime_error(s.str());
+  }catch(const out_of_range& e) {
+    stringstream s;
+    s << "out of bounds environment variable:" << x;
+    throw runtime_error(s.str());
+  }
+}
+#endif
--- message-queue/cpp/src/main.cpp
+++ message-queue/cpp/src/main.cpp
@@ -13,41 +13,10 @@
 #include <optional>
 
 #include <boost/algorithm/string.hpp>
-
-#define VERBOSE(x) if(igetenv("VERBOSE", 0)) osyncstream(cout) << x << endl
-#define INFO(x) osyncstream(cout) << x << endl
-#define ERR(x) osyncstream(cerr) << x << endl
-#define WVERBOSE(id, x) VERBOSE("Worker " << id << ": " << x)
-#define WERR(id, x) ERR("Worker " << id << ": " << x)
-#define THROW(x) { stringstream ss; ss << x; throw runtime_error(ss.str()); }
+#include "./log.cpp"
 
 using namespace std;
 using namespace pqxx;
-
-inline int igetenv(const char* x, int def) {
-  char *c = getenv(x);
-  if(!c)
-    return def; // x undefined
-
-  string s(c);
-  boost::trim(s);
-  if (s.empty()) // x defined empty
-    return def;
-
-  // x defined with some value
-  try {
-    int i = stoi(s);
-    return i;
-  }catch(const invalid_argument& e) {
-    stringstream s;
-    s << "invalid environemnt variable: " << x;
-    throw runtime_error(s.str());
-  }catch(const out_of_range& e) {
-    stringstream s;
-    s << "out of bounds environment variable:" << x;
-    throw runtime_error(s.str());
-  }
-}
 
 class Config {
 public:
