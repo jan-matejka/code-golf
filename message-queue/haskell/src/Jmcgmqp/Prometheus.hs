fa72ee7a3059a4ca3642ff2107e2bea3a89d218a
Jan Matějka <jan@matejka.ninja>
2024-09-25 18:20:50 +0200

    feat(message-queue/haskell): define test metric

--- message-queue/haskell/src/Jmcgmqp/Prometheus.hs
+++ message-queue/haskell/src/Jmcgmqp/Prometheus.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE OverloadedStrings, ImportQualifiedPost #-}
 module Jmcgmqp.Prometheus
 ( cmdTestPrometheus
+, newMetrics
 ) where
+
+import Prometheus qualified as P
+
+newMetrics :: IO [P.Gauge]
+newMetrics = do
+  test <- P.register $ P.gauge (P.Info "test" "Test Metric")
+  return [test]
 
 cmdTestPrometheus :: IO ()
 cmdTestPrometheus = do
