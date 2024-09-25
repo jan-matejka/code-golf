4a4662d79a6386d66e04fe72dc0a49ed90c75235
Jan Matějka <jan@matejka.ninja>
2024-09-25 18:59:14 +0200

    feat(message-queue/haskell): cmdTestPrometheus increments test metric and prints it

--- message-queue/haskell/src/Jmcgmqp/Prometheus.hs
+++ message-queue/haskell/src/Jmcgmqp/Prometheus.hs
@@ -1,16 +1,28 @@
-{-# LANGUAGE OverloadedStrings, ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Jmcgmqp.Prometheus
 ( cmdTestPrometheus
 , newMetrics
+, push
 ) where
 
-import Prometheus qualified as P
+import Prometheus (
+  Gauge, gauge, register, Info(Info),
+  exportMetricsAsText, incGauge
+  )
 
-newMetrics :: IO [P.Gauge]
+newMetrics :: IO [Gauge]
 newMetrics = do
-  test <- P.register $ P.gauge (P.Info "test" "Test Metric")
+  test <- register $ gauge (Info "test" "Test Metric")
   return [test]
 
 cmdTestPrometheus :: IO ()
 cmdTestPrometheus = do
   putStrLn "Test prometheus"
+  m <- newMetrics
+  incGauge $ head m
+  push
+
+push :: IO ()
+push = do
+  samples <- exportMetricsAsText
+  print samples
