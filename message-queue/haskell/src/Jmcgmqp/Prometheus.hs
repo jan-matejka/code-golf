119301d196c5dfb9ebfaeff1cf2decac3c95331f
Jan Matějka <jan@matejka.ninja>
2024-09-26 17:36:18 +0200

    feat(message-queue/haskell): actually push metrics

--- message-queue/haskell/src/Jmcgmqp/Prometheus.hs
+++ message-queue/haskell/src/Jmcgmqp/Prometheus.hs
@@ -5,7 +5,7 @@ module Jmcgmqp.Prometheus
 , push
 ) where
 
-import Data.Map (Map, fromList, union, toList)
+import Data.Map (Map, fromList, union)
 import Data.ByteString.Lazy.Char8 qualified as B
 
 import Prometheus (exportMetricsAsText, incGauge)
@@ -13,6 +13,7 @@ import Prometheus (exportMetricsAsText, incGauge)
 import Jmcgmqp.Runtime (Instance(runtime, metrics), Runtime, runtimeAsMap)
 import Jmcgmqp.Prometheus.Metrics (
   Metrics(test), newMetrics, SampleDesc(SampleDesc), sdescAsMap)
+import Jmcgmqp.Prometheus.Http (pushAdd)
 
 cmdTestPrometheus :: Instance -> IO ()
 cmdTestPrometheus app = do
@@ -23,38 +24,43 @@ cmdTestPrometheus app = do
 push :: Runtime -> SampleDesc -> Int -> IO ()
 push r sdesc wId = do
   samples <- exportMetricsAsText
-  B.putStrLn $ applyLabels labels samples
+  -- pushAdd labelMap $ applyLabels labels samples
+  pushAdd labelMap samples
   where
-    labels :: B.ByteString
-    labels = packLabels $ union
+    labelMap :: Map B.ByteString B.ByteString
+    labelMap = union
       (runtimeAsMap r)
       $ union
         (sdescAsMap sdesc)
         (fromList [("worker_id", B.pack $ show wId)])
 
-packLabels :: Map B.ByteString B.ByteString -> B.ByteString
-packLabels m = wrap (join (map pack $ toList m))
-  where
-    wrap :: B.ByteString -> B.ByteString
-    wrap s = "{" `B.append` s `B.append` "}"
-    join :: [B.ByteString] -> B.ByteString
-    join = B.intercalate ","
-    pack :: (B.ByteString, B.ByteString) -> B.ByteString
-    pack (k, v) = k `B.append` "=\"" `B.append` v `B.append` "\""
-
--- | apply labels to exported unlabeled metrics
--- TBD: Vulnerable to injection
-applyLabels :: B.ByteString -> B.ByteString -> B.ByteString
-applyLabels labels b = B.unlines $  [
-  if i `mod` (3::Int) == 0 then apply line
-  else line
-  | (i, line) <- zip [1..] $ B.lines b
-  ]
-  where
-    apply :: B.ByteString -> B.ByteString
-    apply line = metric `B.append`labels `B.append` " " `B.append` value
-      where
-        metric :: B.ByteString
-        metric = head $ B.words line
-        value :: B.ByteString
-        value = last $ B.words line
+-- -- | Use as::
+-- -- pushAdd labelMap $ applyLabels (packLabels labelMap) samples
+-- --  turns out I need to put them into labels,not body for "push add"
+-- 
+-- packLabels :: Map B.ByteString B.ByteString -> B.ByteString
+-- packLabels m = wrap (join (map pack $ toList m))
+--   where
+--     wrap :: B.ByteString -> B.ByteString
+--     wrap s = "{" `B.append` s `B.append` "}"
+--     join :: [B.ByteString] -> B.ByteString
+--     join = B.intercalate ","
+--     pack :: (B.ByteString, B.ByteString) -> B.ByteString
+--     pack (k, v) = k `B.append` "=\"" `B.append` v `B.append` "\""
+-- 
+-- -- | apply labels to exported unlabeled metrics
+-- -- Note: vulnerable to injection, use URI.encode or smth
+-- applyLabels :: B.ByteString -> B.ByteString -> B.ByteString
+-- applyLabels labels b = B.unlines $  [
+--   if i `mod` (3::Int) == 0 then apply line
+--   else line
+--   | (i, line) <- zip [1..] $ B.lines b
+--   ]
+--   where
+--     apply :: B.ByteString -> B.ByteString
+--     apply line = metric `B.append`labels `B.append` " " `B.append` value
+--       where
+--         metric :: B.ByteString
+--         metric = head $ B.words line
+--         value :: B.ByteString
+--         value = last $ B.words line
---
+++ message-queue/haskell/src/Jmcgmqp/Prometheus/Http.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+module Jmcgmqp.Prometheus.Http
+( pushAdd
+) where
+
+import Data.Map (Map, toList)
+import Data.List (intercalate)
+import Data.ByteString.Lazy.Char8 (ByteString, unpack)
+
+import Network.URI.Encode (encode)
+import Network.HTTP.Client (
+  newManager, parseRequest, defaultManagerSettings, httpLbs, responseStatus,
+  method, requestBody, RequestBody(RequestBodyLBS)
+  )
+import Network.HTTP.Types.Status (statusCode)
+
+mkURI :: Map ByteString ByteString -> String
+mkURI labels =
+  "http://localhost:9091/metrics/job/mq-producer/" ++
+    (intercalate "/" $ map item $ toList labels)
+  where
+    item :: (ByteString, ByteString) -> String
+    item (k, v) = (encode $ unpack k) ++ "/" ++ (encode $ unpack v)
+
+pushAdd :: Map ByteString ByteString -> ByteString -> IO ()
+pushAdd labels body = do
+  manager <- newManager defaultManagerSettings
+
+  iReq <- parseRequest $ mkURI labels
+  let req = iReq { method = "POST", requestBody = RequestBodyLBS body }
+  resp <- httpLbs req manager
+  if resp.responseStatus.statusCode /= 200 then error $ show resp
+  else return ()
