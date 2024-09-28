de6bdc80db24cd39a4a2182c9c4f2205f1535195
Jan Matějka <jan@matejka.ninja>
2024-09-28 12:11:39 +0200

    feat(message-queue/python): add setup.py

---
+++ message-queue/python/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import sys
+
+from setuptools import setup
+
+CURRENT_PYTHON = sys.version_info[:2]
+REQUIRED_PYTHON = (3, 11)
+
+if CURRENT_PYTHON < REQUIRED_PYTHON:
+    sys.stderr.write("Unsupported Python version {}.{}".format(*CURRENT_PYTHON))
+    sys.exit(1)
+
+requires = [
+    "psycopg>=3.1.7,<4",
+    "prometheus-client>=0.16.0,<1",
+]
+test_requirements = [
+    "pytest>=7.2.1",
+]
+
+setup(
+    name='jmcgmqp',
+    version='0.1.0',
+    description='',
+    packages=["jmcgmqp"],
+    package_dir={"": "src"},
+    python_requires=">={}.{}".format(*REQUIRED_PYTHON),
+    install_requires=requires,
+    tests_require=test_requirements,
+)
