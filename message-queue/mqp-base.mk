7bb70dddbdafebd5681fdb57e09db5a5ff016e8a
Jan Matějka <jan@matejka.ninja>
2024-09-29 17:12:16 +0200

    feat(message-queue): split haskell/GNUmakefile into mqp-base.mk for generic usage
    Also rename some targets to more shorter, more fitting names; making a
    distinction between container and its image.

---
+++ message-queue/mqp-base.mk
@@ -0,0 +1,41 @@
+# Base GNU makefile for building individual implementations and
+# their container images and container.
+.DEFAULT_TARGET = build
+CONTAINERFILE := Containerfile
+COMMON_NAME := mq-producer
+NAME := $(shell basename $(CURDIR))
+FULL_NAME := $(COMMON_NAME)-$(NAME)
+
+.PHONY: image
+image: ## Build image
+
+	podman build -f $(CONTAINERFILE) -t localhost/$(FULL_NAME)
+
+.PHONY: container
+container: ## Run the image in container
+
+	podman rm $(FULL_NAME) || true
+	# Run with --network=host for now.
+	# Option --userns and --pod are mutually exlusive.
+	# TBD Maybe its possible to run the entire pod with --userns.
+	podman run \
+		--name $(FULL_NAME) \
+		--userns keep-id \
+		-v $(realpath ../../):/home/user/code-golf \
+		-u user \
+		--network=host \
+		-it \
+		$(FULL_NAME) \
+		zsh
+
+.PHONY: exec-root
+exec-root: ## Exec into running container as root
+
+	podman exec -u root -it $(FULL_NAME) bash
+
+.PHONY: help
+help: ## Print help
+
+	@@grep -h '^\([a-zA-Z]\+\):' $(MAKEFILE_LIST) | \
+		sort | \
+		awk -F ':.*?## ' 'NF==2 {printf "  %-26s%s\n", $$1, $$2}'
