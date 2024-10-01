7bf8fee6b9d46054af28fd68597bcc9b1a1ac997
Jan Matějka <jan@matejka.ninja>
2024-10-01 15:08:42 +0200

    feat(message-queue/python): exceptions raised by observers are logged and ignored

--- message-queue/python/src/jmcgmqp/observer.py
+++ message-queue/python/src/jmcgmqp/observer.py
@@ -1,19 +1,27 @@
 from dataclasses import dataclass
 from typing import Callable
+import logging
 
 from jmcgmqp.event import Event
 
+log = logging.getLogger(__name__)
+
 Observer = Callable[[Event], None]
 
-@dataclass
 class Registry:
+    _log: logging.Logger = None
     observers = None
-    def __post_init__(self):
+
+    def __init__(self, _log=log):
+        self._log = _log
         self.observers = []
 
     def subscribe(self, o: Observer):
         self.observers.append(o)
 
-    def publish(self, e):
+    def publish(self, event):
         for o in self.observers:
-            o(e)
+            try:
+                o(event)
+            except Exception as e:
+                self._log.exception(e)
