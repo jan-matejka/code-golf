5341acb66ff3889a5e878b9edd282d84bf2af1fa
Jan Matějka <jan@matejka.ninja>
2024-10-03 13:36:29 +0200

    feat(filesync/python): collect actions to perform

--- filesync/python/src/jmcgfs/main.py
+++ filesync/python/src/jmcgfs/main.py
@@ -1,12 +1,30 @@
 import argparse
 from collections.abc import Sequence
+from dataclasses import dataclass
 import logging
+import os
 from pathlib import Path
 import sys
+from typing import Optional
 
-
+@dataclass
 class Action:
     pass
+
+@dataclass
+class CopyFile(Action):
+    src: Path
+    dst: Path
+
+@dataclass
+class CopyDir(Action):
+    src: Path
+    dst: Path
+
+@dataclass
+class Ignore(Action):
+    src: Path
+    reason: str
 
 def _check_dir(p: Path, name: str) -> None:
     """
@@ -17,10 +35,54 @@ def _check_dir(p: Path, name: str) -> None:
     if not p.is_dir():
         raise RuntimeError(f"not a directory: {name}: {p}")
 
-def collect(s: Path, r: Path) -> Sequence[Action]:
+def is_unsupported(p: Path) -> Optional[str]:
+    """
+    :returns: None if path is of supported file type. Otherwise string identifying the unsupported
+        file type.
+    """
+    if p.is_symlink():
+        return "symlink"
+    if p.is_block_device():
+        return "block_device"
+    if p.is_char_device():
+        return "char_device"
+    if p.is_fifo():
+        return "fifo"
+    if p.is_socket():
+        return "socket"
+    if p.is_mount():
+        return "mount"
+    return None
+
+def collect(s: Path, r: Path, _is_unsupported=is_unsupported) -> Sequence[Action]:
     _check_dir(s, "source")
     _check_dir(r, "replica")
-    return []
+
+    s = s.absolute()
+    r = r.absolute()
+
+    actions = []
+    for dirpath, dirnames, filenames in os.walk(str(s)):
+        for x in filenames:
+            src = Path(dirpath) / x
+            if src.is_file() and not src.is_symlink():
+                dst = r / src.relative_to(s)
+                actions.append(CopyFile(src, dst))
+            else:
+                typ = _is_unsupported(src)
+                if not typ:
+                    if not src.exists():
+                        typ = "file no longer exists"
+                    else:
+                        typ = "unknown"
+                actions.append(Ignore(src, typ))
+
+        for x in dirnames:
+            src = Path(dirpath) / x
+            dst = r / src.relative_to(s)
+            actions.append(CopyDir(src, dst))
+
+    return actions
 
 def main(argv=sys.argv, _collect=collect):
     p = argparse.ArgumentParser()
--- filesync/python/test/test_main.py
+++ filesync/python/test/test_main.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 import tempfile
 
-from jmcgfs.main import main, collect
+from jmcgfs.main import main, collect, is_unsupported, CopyFile, CopyDir, Ignore
 
 import pytest
 from pytest import raises
-from unittest.mock import Mock
+from unittest.mock import Mock, create_autospec
 
 def test_main():
     m = Mock()
@@ -60,5 +60,45 @@ def test_collect_raises_not_dir_r(s):
         collect(s, p)
     assert str(e.value) == f"not a directory: replica: {p}"
 
+unsupported = "symlink block_device char_device fifo socket mount".split(" ")
+@pytest.mark.parametrize("name", unsupported)
+def test_is_unsupported(name):
+    m = create_autospec(Path, instance=True, spec_set=True)
+    for method in [getattr(m, f"is_{x}") for x in unsupported]:
+        method.return_value = False
+    assert is_unsupported(m) == None
+    getattr(m, f"is_{name}").return_value = True
+    assert is_unsupported(m) == name
+
 def test_collect(s, r):
-    assert collect(s, r) == []
+    (s / "foo").touch()
+    (s / "bar/qux").mkdir(parents=True)
+    (s / "bar/qux/a").touch()
+    (s / "bar/qux/b").touch()
+    (s / "bar/s").symlink_to('x')
+    (s / "bar/s2").symlink_to('qux/a')
+
+    # Maybe dependent on the filesystem or its options but the order is significant.
+    assert collect(s, r) == [
+        CopyFile(s / "foo", r / "foo"),
+        CopyDir(s / "bar", r / "bar"),
+        Ignore(s / "bar/s", "symlink"),
+        Ignore(s / "bar/s2", "symlink"),
+        CopyDir(s / "bar/qux", r / "bar/qux"),
+        CopyFile(s / "bar/qux/b", r / "bar/qux/b"),
+        CopyFile(s / "bar/qux/a", r / "bar/qux/a"),
+    ]
+
+def test_collect_disappeared(s, r):
+    (s / "foo").symlink_to("bar")
+    assert collect(s, r, _is_unsupported=lambda _: None) == [
+        Ignore(s/"foo", "file no longer exists")
+    ]
+
+def test_collect_unknown(s, r):
+    (s / "foo").symlink_to("bar")
+    (s / "bar").touch()
+    assert collect(s, r, _is_unsupported=lambda _: None) == [
+        CopyFile(s / "bar", r / "bar"),
+        Ignore(s/"foo", "unknown")
+    ]
