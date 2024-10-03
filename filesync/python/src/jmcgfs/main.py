824287907ec1e3baa4a2d2b4c759fe0601be9d7d
Jan Matějka <jan@matejka.ninja>
2024-10-03 11:53:58 +0200

    feat(filesync/python): add logging

--- filesync/python/src/jmcgfs/main.py
+++ filesync/python/src/jmcgfs/main.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 from pathlib import Path
 
 def main():
@@ -8,7 +9,13 @@ def main():
     p.add_argument("-l", "--logfile", help="Logfile", type=Path, required=False)
     p.add_argument("-i", "--interval", help="seconds", type=int, required=True)
     args = p.parse_args()
-    print(args)
+
+    log = logging.getLogger(__name__)
+    if args.logfile:
+        logging.basicConfig(filename=args.logfile, level=logging.INFO)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
 
 if __name__ == "__main__":
     main()
