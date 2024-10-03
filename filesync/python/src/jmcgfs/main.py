87a1411b16df1e280119b86091f8add739e7fde4
Jan Matějka <jan@matejka.ninja>
2024-10-03 12:10:33 +0200

    feat(filesync/python): test main

--- filesync/python/src/jmcgfs/main.py
+++ filesync/python/src/jmcgfs/main.py
@@ -1,14 +1,18 @@
 import argparse
 import logging
 from pathlib import Path
+import sys
 
-def main():
+def collect(s: Path, r: Path):
+    pass
+
+def main(argv=sys.argv, _collect=collect):
     p = argparse.ArgumentParser()
     p.add_argument("-s", "--source", help="Directory path", type=Path, required=True)
     p.add_argument("-r", "--replica", help="Directory path", type=Path, required=True)
     p.add_argument("-l", "--logfile", help="Logfile", type=Path, required=False)
     p.add_argument("-i", "--interval", help="seconds", type=int, required=True)
-    args = p.parse_args()
+    args = p.parse_args(argv[1:])
 
     log = logging.getLogger(__name__)
     if args.logfile:
@@ -16,6 +20,7 @@ def main():
     else:
         logging.basicConfig(level=logging.INFO)
 
+    _collect(args.source, args.replica)
 
-if __name__ == "__main__":
+if __name__ == "__main__": # pragma: nocover
     main()
--- filesync/python/test/test_main.py
+++ filesync/python/test/test_main.py
@@ -1,4 +1,14 @@
-import jmcgfs.main
+from pathlib import Path
 
-def test_pass():
-    pass
+from jmcgfs.main import main
+
+from unittest.mock import Mock
+
+def test_main():
+    m = Mock()
+    main(argv="0 -s a -r b -i 5".split(" "), _collect=m)
+    m.assert_called_once_with(Path("a"), Path("b"))
+
+    m.reset_mock()
+    main(argv="0 -s a -r b -i 5 -l log".split(" "), _collect=m)
+    m.assert_called_once_with(Path("a"), Path("b"))
