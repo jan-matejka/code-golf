163503e277e4b8ed742f2d99898542cbfcc23d80
Jan Matějka <jan@matejka.ninja>
2024-11-11 12:55:04 +0100

    phonebook/python/main.py: define interface

---
+++ phonebook/python/main.py
@@ -0,0 +1,10 @@
+class Phonebook:
+    def search_contact(self, name: str) -> list[str]:
+        """
+        :returns: numbers associated with contacts starting on `name`.
+        """
+
+    def add_contact(self, name: str, number: str) -> bool:
+        """
+        Registers contact's `number` under given `name`.
+        """
