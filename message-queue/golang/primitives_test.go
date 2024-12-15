1e85641c40d39d8df50bb408bbfbe63eca5568c4
Jan Matějka <jan@matejka.ninja>
2024-12-15 20:33:01 +0100

    message-queue/golang: mps calculation refs #30

--- message-queue/golang/primitives_test.go
+++ message-queue/golang/primitives_test.go
@@ -5,8 +5,24 @@ import "testing"
 func TestMessagesPerSecond(t *testing.T) {
 	rs := NewResults()
 	rs.Add(NewWorkerResult(1, 10, 1_000_000_000))
+	rs.Add(NewWorkerResult(2, 10, 1_000_000_000))
+	if rs.MessagesPerSecond != 20 {
+		t.Fatalf("Expected %v, got %v", 20, rs.MessagesPerSecond)
+	}
+
+	rs = NewResults()
+	rs.Add(NewWorkerResult(1, 10, 1_000_000_000))
+	rs.Add(NewWorkerResult(2, 10, 1_000_000_000))
+	rs.Add(NewWorkerResult(3, 10, 1_000_000_000))
+	rs.Add(NewWorkerResult(4, 10, 1_000_000_000))
+	if rs.MessagesPerSecond != 40 {
+		t.Fatalf("Expected %v, got %v", 40, rs.MessagesPerSecond)
+	}
+
+	rs = NewResults()
+	rs.Add(NewWorkerResult(1, 10, 1_000_000_000))
 	rs.Add(NewWorkerResult(2, 20, 2_000_000_000))
-	if rs.MessagesPerSecond != 10 {
-		t.Fatalf("Expected %v, got %v", 10, rs.MessagesPerSecond)
+	if rs.MessagesPerSecond != 20 {
+		t.Fatalf("Expected %v, got %v", 20, rs.MessagesPerSecond)
 	}
 }
