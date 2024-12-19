61af2393c06de13b27d7d0d285e12d3ae9455095
Jan Matějka <jan@matejka.ninja>
2024-12-19 10:21:38 +0100

    message-queue/haskell: wip

--- message-queue/haskell/src/Jmcgmqp/Postgres.hs
+++ message-queue/haskell/src/Jmcgmqp/Postgres.hs
@@ -1,19 +1,23 @@
-module Jmcgmqp.Prometheus (
+module Jmcgmqp.Postgres (
   Postgres(..)
 ) where
 
 import Database.PostgreSQL.Simple (
   Connection, execute, Only(Only), connectPostgreSQL)
 
+import Jmcgmqp.Runtime (Runtime)
+import Jmcgmqp.Prometheus.Metrics (SampleDesc)
+import Jmcgmqp.Worker (Results)
+
 newtype Postgres = Postgres {
   conn :: Connection
 }
 
-newPostgres :: String -> Postgres
+newPostgres :: String -> IO Postgres
 newPostgres = Postgres . connectPostgreSQL
 
 push :: Postgres -> Runtime -> SampleDesc -> Results -> IO ()
 push pg r sdesc rs = do
   runtime_id <- createRuntime
   sample_id < createSample
-  _ <- mapIO (createWorker . sample_id) rs.workers
+  mapM_ (createWorker . sample_id) rs.workers
---
+++ message-queue/haskell/src/test/PostgresSpec.hs
@@ -0,0 +1,36 @@
+module PostgresSpec
+( spec )
+where
+
+import Test.Hspec
+import Test.QuickCheck
+
+import System.Clock (TimeSpec(..))
+import Control.Monad (void)
+
+import Database.PostgreSQL.Simple (execute, connectPostgreSQL)
+
+import Jmcgmqp.Prometheus.Metrics (SampleDesc(SampleDesc))
+import Jmcgmqp.Worker
+import Jmcgmqp.Postgres
+
+mkDb :: IO ()
+mkDb = do
+  c <- connectPostgreSQL "postgres://postgres@localhost:5433"
+  void . execute c "drop database if exists test"
+  void . execute c "create database test template mq"
+
+spec :: Spec
+spec = do
+  describe "Postgres.push" $ do
+    it "works" $ do
+      let sdesc = SampleDesc 2 "forkIO" "postgres"
+      let rs = newResults [
+               newWorkerResult 1 10 $ TimeSpec 1 0
+             , newWorkerResult 2 20 $ TimeSpec 2 0
+             ]
+      mkDb
+      pg <- newPostgres
+      r <- newRuntime
+      push pg r sdesc rs
+      0 `shouldBe` 1
