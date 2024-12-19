2863c2c7c2d7d2a7769e0107a35d6f60d57cbb9c
Jan Matějka <jan@matejka.ninja>
2024-12-19 10:00:51 +0100

    feat(message-queue/haskell): automatic spec discovery
    closes #33

--- message-queue/haskell/src/test/Main.hs
+++ message-queue/haskell/src/test/Main.hs
@@ -1,10 +1,1 @@
-module Main
-( main )
-where
-
-import Test.Hspec
-import Test.QuickCheck
-
-import WorkerSpec
-
-main = hspec $ describe "Worker" WorkerSpec.spec
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
