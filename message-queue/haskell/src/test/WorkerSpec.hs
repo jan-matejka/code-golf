8ae3c7e72e782c63c5160c808593b07d742d9a9a
Jan Matějka <jan@matejka.ninja>
2024-12-20 14:37:15 +0100

    feat(message-queue/haskell): -Wall for hspec and fix warnings in WorkerSpec
    refs #40

--- message-queue/haskell/src/test/WorkerSpec.hs
+++ message-queue/haskell/src/test/WorkerSpec.hs
@@ -3,7 +3,6 @@ module WorkerSpec
 where
 
 import Test.Hspec
-import Test.QuickCheck
 
 import System.Clock (TimeSpec(..))
 import Jmcgmqp.Worker
@@ -18,16 +17,18 @@ spec = do
       newStart 3 `shouldBe` Just 5
 
   describe "Results" $ do
-    it "calculates mps" $ do
+    it "calculates mps 1" $ do
       let rs = newResults [newWorkerResult 1 10 $ TimeSpec 1 0]
       rs.messagesPerSecond `shouldBe` 10
 
+    it "calculates mps 2" $ do
       let rs = newResults [
              newWorkerResult 1 10 $ TimeSpec 1 0,
              newWorkerResult 2 10 $ TimeSpec 1 0
              ]
       rs.messagesPerSecond `shouldBe` 20
 
+    it "calculates mps 3" $ do
       let rs = newResults [
              newWorkerResult 1 10 $ TimeSpec 1 0,
              newWorkerResult 2 10 $ TimeSpec 1 0,
@@ -36,6 +37,7 @@ spec = do
              ]
       rs.messagesPerSecond `shouldBe` 40
 
+    it "calculates mps 4" $ do
       let rs = newResults [
              newWorkerResult 1 10 $ TimeSpec 1 0,
              newWorkerResult 2 20 $ TimeSpec 2 0
