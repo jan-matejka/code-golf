ba1665c9236affb99c909b4b2652a372b81ebcc8
Jan Matějka <jan@matejka.ninja>
2024-12-27 14:03:30 +0100

    feat(message-queue/cpp): use PG_TEST_DSN env var
    refs: #60

--- message-queue/cpp/src/pg_test.cpp
+++ message-queue/cpp/src/pg_test.cpp
@@ -1,7 +1,23 @@
 #include "pg_test.hpp"
+#include "log.hpp"
+#include <fmt/format.h>
+
+TestConfig::TestConfig()
+: PG_TEST_DSN(sgetenv("PG_TEST_DSN", "localhost:5433"))
+, PG_TEST_ROOT_DSN(fmt::format("postgres://postgres@{}", PG_TEST_DSN))
+, PG_TEST_MQ_DSN(fmt::format("postgres://mq@{}/test", PG_TEST_DSN))
+{
+};
+
+const TestConfig& TestConfig::Instance() {
+  static TestConfig i;
+  return i;
+}
+
+const TestConfig TCG();
 
 PgFixture::PgFixture()
-: root_conn("postgres://postgres@localhost:5433"),
+: root_conn(TestConfig::Instance().PG_TEST_ROOT_DSN),
   dsn(CreateTestDb())
 {
 };
@@ -10,7 +26,7 @@ string PgFixture::CreateTestDb() {
   nontransaction tx(root_conn);
   tx.exec("drop database if exists test");
   tx.exec("create database test template mq");
-  return string("postgres://mq@localhost:5433/test");
+  return string(TestConfig::Instance().PG_TEST_MQ_DSN);
 };
 
 PgFixture::~PgFixture() {
--- message-queue/cpp/src/pg_test.hpp
+++ message-queue/cpp/src/pg_test.hpp
@@ -1,10 +1,20 @@
 #ifndef PG_UNITTEST_CPP
 #define PG_UNITTEST_CPP
 
+#include <string>
 #include <pqxx/pqxx>
 
 using namespace std;
 using namespace pqxx;
+
+class TestConfig {
+  TestConfig();
+public:
+  string PG_TEST_DSN;
+  string PG_TEST_ROOT_DSN;
+  string PG_TEST_MQ_DSN;
+  static const TestConfig& Instance();
+};
 
 class PgFixture {
   connection root_conn;
