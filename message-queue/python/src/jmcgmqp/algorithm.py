69528e3c6b719f2631ef6e32223731509223bb02
Jan Matějka <jan@matejka.ninja>
2025-01-30 21:07:24 +0100

    refactor(message-queue/python): SampleIterator
    refs: #69

--- message-queue/python/src/jmcgmqp/algorithm.py
+++ message-queue/python/src/jmcgmqp/algorithm.py
@@ -1,9 +1,67 @@
+from collections.abc import Iterator
 from dataclasses import dataclass
 from typing import *
 import itertools
 
 T = TypeVar('T')
 Sampler = Callable[[int],[T]]
+
+@dataclass
+class SampleIterator(Iterator):
+    """
+    Basicly the same as :ref:`SampleGenerator` except it is an Iterator.
+
+    It's ugly, it's harder to write, and harder to read. But it poses an
+    interesting question of wheter iterator or generator is preferred and if it
+    improves if we move campler call outside and turns this into sampler
+    observer.
+    """
+    sampler: Sampler
+    power:int = 0
+    step: int | None = None
+    max_step: int | None = None
+
+    _prev:T | None = None
+    _next_impl: Callable[[], [T]] = None
+
+    def __post_init__(self):
+        self._next_impl = self._next_power
+
+    def __next__(self):
+        # Note: we can not simply re-assign __next__ itself because next()
+        # resolves it statically via Py_TYPE() to the class method (probably
+        # for performance reasons).
+        return self._next_impl()
+
+    def _next_power(self):
+        r = self.sampler(2**self.power)
+        if self._prev and self._prev >= r:
+            self.step = 2**(self.power - 1) + 1
+            self.max_step = 2**self.power
+            self._next_impl = self._next_step
+        else:
+            self.power += 1
+            self._prev = r
+        return r
+
+    def _next_step(self):
+        if self.step == self.max_step:
+            self._next_impl = self._next_stop
+            raise StopIteration()
+
+        r = self.sampler(self.step)
+        if self._prev and self._prev >= r:
+            self._next_impl = self._next_stop
+        else:
+            self._prev = r
+            self.step += 1
+        return r
+
+    def _next_stop(self):
+        raise StopIteration()
+
+    def __iter__(self):
+        return self
 
 def SampleGenerator(
     sample: Sampler,
--- message-queue/python/test/test_algorithm.py
+++ message-queue/python/test/test_algorithm.py
@@ -1,18 +1,27 @@
-from jmcgmqp.algorithm import find_maximum, Sampler
+from jmcgmqp.algorithm import find_maximum, Sampler, SampleIterator
 
 from unittest.mock import create_autospec, call
 import pytest
 
-@pytest.mark.parametrize('sequence, result, args, power', (
-    ((1, 1, 1), 1, (1, 2), 0),
+cases = (
+    ((1, 1), 1, (1, 2), 0),
     ((1, 2, 1, 1), 2, (1, 2, 4, 3), 0),
     ((1, 2, 1, 3), 3, (1, 2, 4, 3), 0),
     ((1, 2, 3, 4, 5, 1, 1), 5, (1, 2, 4, 8, 16, 32, 17), 0),
     ((1, 2, 3, 1, 4, 5, 1), 5, (1, 2, 4, 8, 5, 6, 7), 0),
     ((1, 1, 1), 1, (4, 8, 5), 2),
-))
+)
+
+@pytest.mark.parametrize('sequence, result, args, power', cases)
 def test_find_maximum(sequence, result, args, power):
     sample = create_autospec(Sampler, spec_set=True)
     sample.side_effect = sequence
     assert find_maximum(sample, power) == result
+    assert sample.call_args_list == [call(x) for x in args]
+
+@pytest.mark.parametrize('sequence, result, args, power', cases)
+def test_SampleIterator(sequence, result, args, power):
+    sample = create_autospec(Sampler, spec_set=True)
+    sample.side_effect = sequence
+    assert tuple(SampleIterator(sample, power)) == sequence
     assert sample.call_args_list == [call(x) for x in args]
