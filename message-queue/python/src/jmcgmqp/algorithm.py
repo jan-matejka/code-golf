411b905221170d848138f3d8ba87a7d35db4dc65
Jan Matějka <jan@matejka.ninja>
2025-01-30 20:16:11 +0100

    refactor(message-queue/python): refactor find_maximum into max(SampleGenerator(...))
    closes: #69

--- message-queue/python/src/jmcgmqp/algorithm.py
+++ message-queue/python/src/jmcgmqp/algorithm.py
@@ -1,30 +1,50 @@
+from dataclasses import dataclass
 from typing import *
 import itertools
 
 T = TypeVar('T')
 Sampler = Callable[[int],[T]]
 
-def find_maximum(
+def SampleGenerator(
     sample: Sampler,
     starting_power: int = 0,
-) -> T:
+) -> Generator[T, None, None]:
     """
     Run `sample` with increasing powers until its result decreases.
-    Then run `sample` with increments from last input that returned non-decreasing value.
+    Then run `sample` with increments from last input that returned
+    non-decreasing value.
 
-    :returns: the last non-decreasing result.
+    Yields the sampling results.
     """
+    # I'm not sure this is any better. Sure, we could have simpler unit test
+    # for SampleGenerator and separate one for find_maximum, if we have the
+    # find_maximum facade at all. But is it worth?
+    #
+    # We could also look into refactoring the sample out by sending the result
+    # back into the generator.
+
     prev = None
     for i in itertools.count(starting_power):
         r = sample(2**i)
         if prev and prev >= r:
+            yield r
             break
         prev = r
+        yield r
 
     for j in range(2**(i-1)+1, 2**(i)):
         r = sample(j)
         if prev and prev >= r:
+            yield r
             break
-        prev = r
+        yield r
 
-    return prev
+def find_maximum(sample: Sampler, starting_power: int = 0) -> T:
+    """
+    Run `sample` with increasing powers until its result decreases.
+    Then run `sample` with increments from last input that returned
+    non-decreasing value.
+
+    :returns: the last non-decreasing result.
+    """
+    return max(SampleGenerator(sample, starting_power))
