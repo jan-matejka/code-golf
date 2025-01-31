89748a45e5e15f2330701901a052d7338826ba0d
Jan Matějka <jan@matejka.ninja>
2025-01-31 11:45:45 +0100

    refactor(message-queue/python): test cases definition
    refs: #69

--- message-queue/python/test/test_algorithm.py
+++ message-queue/python/test/test_algorithm.py
@@ -1,27 +1,44 @@
 from jmcgmqp.algorithm import find_maximum, Sampler, SampleIterator
 
+from collections import OrderedDict
 from unittest.mock import create_autospec, call
 import pytest
 
 cases = (
-    ((1, 1), 1, (1, 2), 0),
-    ((1, 2, 1, 1), 2, (1, 2, 4, 3), 0),
-    ((1, 2, 1, 3), 3, (1, 2, 4, 3), 0),
-    ((1, 2, 3, 4, 5, 1, 1), 5, (1, 2, 4, 8, 16, 32, 17), 0),
-    ((1, 2, 3, 1, 4, 5, 1), 5, (1, 2, 4, 8, 5, 6, 7), 0),
-    ((1, 1, 1), 1, (4, 8, 5), 2),
+    (OrderedDict(((1, 1), (2, 1))), 1, 0),
+    (OrderedDict(((1, 1), (2, 2), (4, 1), (3, 1))), 2, 0),
+    (OrderedDict(((1, 1), (2, 2), (4, 1), (3, 3))), 3, 0),
+    (
+        OrderedDict((
+            (1, 1),
+            (2, 2),
+            (4, 3),
+            (8, 4),
+            (16, 5),
+            (32, 1),
+            (17, 1),
+        )),
+        5,
+        0,
+    ),
+    (
+        OrderedDict(((1, 1), (2, 2), (4, 3), (8, 1), (5, 4), (6, 5), (7, 1))),
+        5,
+        0
+    ),
+    (OrderedDict(((4, 1), (8, 1), (5, 1))), 1, 2),
 )
 
-@pytest.mark.parametrize('sequence, result, args, power', cases)
-def test_find_maximum(sequence, result, args, power):
+@pytest.mark.parametrize('sequence, result, power', cases)
+def test_find_maximum(sequence, result, power):
     sample = create_autospec(Sampler, spec_set=True)
-    sample.side_effect = sequence
+    sample.side_effect = list(sequence.values())
     assert find_maximum(sample, power) == result
-    assert sample.call_args_list == [call(x) for x in args]
+    assert sample.call_args_list == [call(x) for x in sequence.keys()]
 
-@pytest.mark.parametrize('sequence, result, args, power', cases)
-def test_SampleIterator(sequence, result, args, power):
+@pytest.mark.parametrize('sequence, result, power', cases)
+def test_SampleIterator(sequence, result, power):
     sample = create_autospec(Sampler, spec_set=True)
-    sample.side_effect = sequence
-    assert tuple(SampleIterator(sample, power)) == sequence
-    assert sample.call_args_list == [call(x) for x in args]
+    sample.side_effect = list(sequence.values())
+    assert tuple(SampleIterator(sample, power)) == tuple(sequence.values())
+    assert sample.call_args_list == [call(x) for x in sequence.keys()]
